7d38180ffac415291c738720e2377d5a11557410
tharuk999 <tharukran@gmail.com>
2026-02-27 17:24:59 -0500

    distinct skills indexed

--- main.py
+++ main.py
@@ -11,16 +11,23 @@ def get_skills(str):
     str = str[1:ind]
     return str.replace(" ", "").split(",")
 
-set = set()
+total_skills = []
+# set = set()
+
 with open(path, "r") as file:
     header = file.readline()
     while True:
         s = file.readline()
         if not s:
             break
-        print(s)
+        # print(s)
         skills = get_skills(s)
+        dic = {'DeepLearning':0, 'Cybersecurity':0, 'SQL':0, 'Networking':0, 'Pytorch':0, 'EthicalHacking':0, 'React':0, 'TensorFlow':0, 'C++':0, 'Python':0, 'Linux':0, 'MachineLearning':0, 'NLP':0, 'Java':0}
         for skill in skills:
             set.add(skill)
+            if skill in dic:
+                dic[skill] += 1
+        total_skills.append(dic)
+        # print(dic)
 
-print(set)
+# print(set)
